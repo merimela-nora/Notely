34ff59fec671697446b23a1c30c49319238570b4
Claire <Clairenjemu@gmail.com>
2025-07-28 14:02:18 +0300

    this is changing few things

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://notely-o6vj.vercel.app",
     credentials: true,
     methods: ["POST", "DELETE", "GET", "PUT", "PATCH"],
   })
